52350ae7d9baf64e6716443cccd0ef3f3e61ec0a
Nareshkrishnaa <nareshkrishnaaa@gmail.com>
2023-05-26 03:58:16 +0530

    Bootstrap css framework added for result page

--- src/main/java/com/naresh/examresult/controller/StudentViewController.java
+++ src/main/java/com/naresh/examresult/controller/StudentViewController.java
@@ -3,19 +3,36 @@ package com.naresh.examresult.controller;
 
 import com.naresh.examresult.entity.Student;
 
+import com.naresh.examresult.service.StudentService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+
 @Controller
+@AllArgsConstructor
 public class StudentViewController {
 
+    private StudentService studentService;
     @PostMapping("/view/post_student")
     public String createStudent(@RequestBody Student student, Model model) {
 
         model.addAttribute("student", student);
 
         return "student-create";
+    }
+
+    @GetMapping("/view/results")
+    public String getAllStudents(Model model){
+
+
+        List<Student> students = studentService.getAllStudents();
+
+        model.addAttribute("students",students);
+        return "result-page";
     }
 }
