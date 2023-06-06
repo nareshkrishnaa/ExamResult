9f8817ecd495c1884ef2120d0a645406d50dd892
Nareshkrishnaa <nareshkrishnaaa@gmail.com>
2023-06-06 23:43:45 +0530

    test cases tested with assert method

--- src/test/java/com/naresh/examresult/service/Impln/StudentServiceImplnTest.java
+++ src/test/java/com/naresh/examresult/service/Impln/StudentServiceImplnTest.java
@@ -6,25 +6,47 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.naresh.examresult.entity.Student;
 import com.naresh.examresult.entity.StudentDto;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.List;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.config.additional-location=classpath:application-test.properties")
 class StudentServiceImplnTest {
 
     @Autowired StudentServiceImpln studentServiceImpln;
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    @BeforeEach
+    void clearDataBase(){
+        jdbcTemplate.execute("delete from student_table");
+        //before each testcase this method will run
+        //we're going to clear database before running each testcase
+    }
     @Test
     void loginStudentWithAValidEntry() {
 
         System.out.println("-----------------------------------------");
 
-        var loggedInStudent = studentServiceImpln.loginStudent(2, "pwd");
+        Student student = new Student();
+        student.setName("Bommi");
+        student.setPassword("pwd1");
+        student.setMath(45);
+        student.setScience(55);
+        student.setEnglish(65);
+        StudentDto savedStudentDto = studentServiceImpln.createStudent(student);
+        StudentDto loggedInStudent = studentServiceImpln.loginStudent(savedStudentDto.getRollNo(), "pwd1");
 
         System.out.println(loggedInStudent.toString());
+        Assertions.assertEquals(savedStudentDto.getRollNo(),loggedInStudent.getRollNo());
+
 
         System.out.println("-----------------------------------------");
     }
@@ -34,7 +56,7 @@ class StudentServiceImplnTest {
 
         System.out.println("-----------------------------------------");
 
-        var loggedInStudent = studentServiceImpln.loginStudent(5, "pwd");
+        var loggedInStudent = studentServiceImpln.loginStudent(5, "pwd1");
 
         System.out.println(loggedInStudent.toString());
 
@@ -45,7 +67,7 @@ class StudentServiceImplnTest {
     void createStudent() {
         System.out.println("-----------------------------------------");
         Student student = new Student();
-        student.setName("Rammi");
+        student.setName("Bommi");
         student.setPassword("pwd1");
         student.setMath(45);
         student.setScience(55);
